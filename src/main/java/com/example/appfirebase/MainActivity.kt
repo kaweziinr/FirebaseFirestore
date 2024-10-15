b2f04709715b2200a5bd7060acb05ea45db51511
kaweziinr <kawe.ribeiro1305@gmail.com>
2024-10-14 22:01:24 -0300

    First commit

---
+++ src/main/java/com/example/appfirebase/MainActivity.kt
@@ -0,0 +1,243 @@
+package com.example.appfirebase;
+
+import android.annotation.SuppressLint
+import android.content.ContentValues
+import android.content.ContentValues.TAG
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.appfirebase.ui.theme.AppFirebaseTheme
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.firestore
+
+
+class MainActivity : ComponentActivity() {
+
+    private val db = Firebase.firestore
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AppFirebaseTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    App(db)
+                }
+            }
+        }
+    }
+}
+
+
+@SuppressLint("UnrememberedMutableState")
+@Composable
+fun App(db: FirebaseFirestore) {
+    var nome by remember {
+        mutableStateOf("")
+    }
+    var telefone by remember {
+        mutableStateOf("")
+    }
+    Column(
+        Modifier
+            .fillMaxWidth()
+    ) {
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .padding(20.dp)
+        ) {
+        }
+        Row(
+            Modifier
+                .fillMaxWidth(),
+            Arrangement.Center
+        ) {
+            Text(text = "App Firebase Firestore")
+        }
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .padding(20.dp)
+        ) {
+        }
+        Row(
+            Modifier
+                .fillMaxWidth()
+        ) {
+            Column(
+                Modifier
+                    .fillMaxWidth(0.3f)
+            ) {
+                Text(text = "Nome:")
+            }
+            Column {
+                TextField(
+                    value = nome,
+                    onValueChange = { nome = it }
+                )
+            }
+        }
+        Row(
+            Modifier
+                .fillMaxWidth()
+        ) {
+            Column(
+                Modifier
+                    .fillMaxWidth(0.3f)
+            ) {
+                Text(text = "Telefone:")
+            }
+            Column {
+                TextField(
+                    value = telefone,
+                    onValueChange = { telefone = it }
+                )
+            }
+        }
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .padding(20.dp)
+        ) {
+        }
+        Row(
+            Modifier
+                .fillMaxWidth(),
+            Arrangement.Center
+        ) {
+            Button(onClick = {
+                val pessoas = hashMapOf(
+                    "nome" to nome,
+                    "telefone" to telefone
+                )
+
+                db.collection("Clientes").add(pessoas)
+                    .addOnSuccessListener { documentReference ->
+                        Log.d(TAG, "DocumentSnapshot written with ID: ${documentReference.id}")
+                    }
+                    .addOnFailureListener { e ->
+                        Log.w(TAG, "Error adding document", e)
+                    }
+            }) {
+                Text(text = "Cadastrar")
+            }
+
+        }
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .padding(20.dp)
+        ) {
+
+        }
+        Row(
+            Modifier
+                .fillMaxWidth()
+        ) {
+            Column(
+                Modifier
+                    .fillMaxWidth(0.5f)
+            ) {
+                Text(text = "Nome:")
+            }
+            Column(
+                Modifier
+                    .fillMaxWidth(0.5f)
+            ) {
+                Text(text = "Telefone:")
+            }
+        }
+
+        // Adicionando a listagem com exclusão
+        Row(
+            Modifier
+                .fillMaxWidth()
+        ) {
+            Column {
+                val clientes = mutableStateListOf<HashMap<String, String>>()
+                val clienteIds = mutableStateListOf<String>() // Lista para armazenar os IDs dos clientes
+
+                // Carregar os clientes do Firestore
+                db.collection("Clientes")
+                    .get()
+                    .addOnSuccessListener { documents ->
+                        for (document in documents) {
+                            val lista = hashMapOf(
+                                "nome" to "${document.data["nome"]}",
+                                "telefone" to "${document.data["telefone"]}"
+                            )
+                            clientes.add(lista)
+                            clienteIds.add(document.id) // Armazenar o ID do cliente
+                        }
+                    }
+                    .addOnFailureListener { exception ->
+                        Log.w(TAG, "Error getting documents: ", exception)
+                    }
+
+                // Exibição da lista de clientes
+                LazyColumn(modifier = Modifier.fillMaxWidth()) {
+                    itemsIndexed(clientes) { index, cliente ->
+                        Row(modifier = Modifier.fillMaxWidth()) {
+                            Column(modifier = Modifier.weight(0.4f)) {
+                                Text(text = cliente["nome"] ?: "--")
+                            }
+                            Column(modifier = Modifier.weight(0.4f)) {
+                                Text(text = cliente["telefone"] ?: "--")
+                            }
+                            Column(modifier = Modifier.weight(0.2f)) {
+                                // Botão de exclusão
+                                Button(onClick = {
+                                    val clienteId = clienteIds[index] // Obter o ID do cliente
+                                    db.collection("Clientes").document(clienteId)
+                                        .delete()
+                                        .addOnSuccessListener {
+                                            Log.d(TAG, "DocumentSnapshot successfully deleted!")
+                                            clientes.removeAt(index)
+                                            clienteIds.removeAt(index)
+                                        }
+                                        .addOnFailureListener { e ->
+                                            Log.w(TAG, "Error deleting document", e)
+                                        }
+                                }) {
+                                    Text("Excluir")
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
